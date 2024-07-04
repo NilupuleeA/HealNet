82c6d8563dc31224be27d8e0847042d716c20374
Nilupulee Amarathunga <153465850+NilupuleeA@users.noreply.github.com>
2024-07-04 11:11:15 +0530

    Change home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-//import 'package:healnet/authentication/signup_screen.dart';
-import 'package:healnet/pages/home_page.dart';
+import 'package:healnet/authentication/signup_screen.dart';
+//import 'package:healnet/pages/home_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,7 +27,7 @@ class MainApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: Colors.black,
       ),
-      home: const HomePage(), // Ensure SignUpScreen is a StatelessWidget or StatefulWidget
+      home: const SignUpScreen(), // Ensure SignUpScreen is a StatelessWidget or StatefulWidget
     );
   }
 }
